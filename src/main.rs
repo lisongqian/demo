28e3bc5f81373608626a08bbad0af360bfd9e90f
Songqian Li <sionli@tencent.com>
2024-06-21 10:38:47 +0800

    test input args
    Signed-off-by: Songqian Li <sionli@tencent.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#[macro_use(crate_authors)]
+extern crate clap;
+
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let app = Command::new("demo").arg(
+        Arg::new("log-file")
+            .long("log-file")
+            .help("Log file. Standard error is used if not specified")
+            .num_args(1)
+            .default_value("/data/log/vmm.log")
+            .group("logging"),
+    );
+    let cmd = app.get_matches();
+    if let Some(file) = cmd.get_one::<String>("log-file") {
+        println!("value: {file:?}")
+    } else {
+        println!("None");
+    }
 }
